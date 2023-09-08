ca6b27f1776648f012d070a02250a17b60d78421
palash04 <palashkamble22@gmail.com>
2023-09-09 01:27:15 +0530

    Update temp.py

--- scripts/mcli_checks/temp.py
+++ scripts/mcli_checks/temp.py
@@ -1,8 +1,12 @@
 import os
+from transformers import AutoTokenizer
 
-variable_value = os.environ.get("foo")
-if variable_value is not None:
-    print(variable_value)
-    print('I am from git')
-else:
-    print("Variable not found")
+# variable_value = os.environ.get("foo")
+# if variable_value is not None:
+#     print(variable_value)
+#     print('I am from git')
+# else:
+#     print("Variable not found")
+
+tokenizer = AutoTokenizer.from_pretrained('Palash123/ola_eng_indic', token='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')
+print(tokenizer)
